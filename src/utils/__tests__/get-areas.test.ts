6304ebc8087f3ae4daa3a8450614388ae34735a9
grami1 <swdevnode@gmail.com>
2023-04-26 10:59:46 +0200

    Area detail page: add tests for api

---
+++ src/utils/__tests__/get-areas.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "@jest/globals";
+import { getAreas } from "../api";
+import axios, { AxiosResponse } from "axios";
+import { Area } from "@/utils/types";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const TOKEN = "token";
+
+describe("getAreas", () => {
+  it("should fetch areas when successful response", async () => {
+    const areas: Area[] = [
+      {
+        areaId: "1",
+        areaName: "myArea",
+      },
+    ];
+    const expectedUrl = "http://localhost:8080/api/v1/areas?userId=1";
+    const expectedOptions = {
+      headers: {
+        Authorization: "Bearer token",
+      },
+    };
+
+    mockedAxios.get.mockResolvedValueOnce({ data: areas });
+
+    const areasResponse: AxiosResponse<Area[]> = await getAreas(1, TOKEN);
+    const actualAreas: Area[] = areasResponse.data;
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(expectedUrl, expectedOptions);
+    expect(actualAreas[0].areaName).toEqual("myArea");
+    expect(actualAreas[0].areaId).toEqual("1");
+  });
+});
